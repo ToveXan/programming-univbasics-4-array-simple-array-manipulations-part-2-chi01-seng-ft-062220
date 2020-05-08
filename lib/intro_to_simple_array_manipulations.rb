b7fc7f6bf02451f1f81207fb012e1c86d0dab0e0
Zoe Xanos <zxanos@gmail.com>
2020-05-08 10:47:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,6 @@
 def using_concat(array, array2)
-  new_array = []
-  
+  array.concat(array2)
 end
 my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 more_favs = ["sports cars", "flatiron school"]
+using_concat(my_favorite_things, more_favs)
