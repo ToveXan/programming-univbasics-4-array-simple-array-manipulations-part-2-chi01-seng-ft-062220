df25e2825904d852dd8298fff5d3c7f59b10a08c
Zoe Xanos <zxanos@gmail.com>
2020-05-08 10:48:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,3 +4,19 @@ end
 my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 more_favs = ["sports cars", "flatiron school"]
 using_concat(my_favorite_things, more_favs)
+
+def using_insert()
+  
+end
+
+def using_uniq()
+end
+
+def using_flatten()
+end
+
+def using_delet()
+end
+
+def using_delete_at()
+end
